1bdd34e89c465c5ec96c13d7a545b257bb86bf97
Eduwin Fabian Torres Mendoza <l20290864@cdguzman.tecnm.mx>
2025-02-18 21:15:53 -0600

    Creacion de clases Empleados para capa de datos y negocios

---
+++ CapaDatos/EmpleadosDAO.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class EmpleadosDAO
+    {
+        private conexion objConexion = new conexion();
+        private MySqlConnection connection;
+        private MySqlCommand cmd;
+        private MySqlDataAdapter adaptador;
+
+        public EmpleadosDAO()
+        {
+            connection = objConexion.Conecta();
+            cmd = new MySqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.StoredProcedure;
+            adaptador = new MySqlDataAdapter(cmd);
+        }
+    }
+}
