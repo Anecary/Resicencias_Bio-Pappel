f8ed9d5d139f7ac28a7d5cebc20590fb956e8367
SamRev2 <L20290850@gdguzman.tecnm.mx>
2025-06-10 16:08:26 -0600

    Tamaño fijo de la pantalla como fullscreen

--- CapaPresentacion/MenuPrincipal.cs
+++ CapaPresentacion/MenuPrincipal.cs
@@ -23,6 +23,7 @@ namespace CapaPresentacion
         public frmMenu()
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
             this.KeyPreview = true;
             this.KeyDown += new KeyEventHandler(FormPrincipal_KeyDown);
             customizeDesign();
@@ -47,9 +48,20 @@ namespace CapaPresentacion
 
 
         }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt32() & 0xFFF0) == SC_MOVE)
+                return;
+
+            base.WndProc(ref m);
+        }
+
         private void frmMenu_Load(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Maximized;
             lblTrabajadores.Text = homeCN.ConcultaNumTrabajadores().Tables["TotalEmpleados"].Rows[0][0].ToString();
             cargarDashboard();
             CargarGraficoCausas();
